40f52064dda6cd1a57480d35aa27f71f328d589d
Denise De Leon <129819435+ardy-dl@users.noreply.github.com>
2023-12-13 16:06:05 +0800

    Update stack_operations.py

--- flask_portfolio-main/stack_operations.py
+++ flask_portfolio-main/stack_operations.py
@@ -6,6 +6,7 @@ class Node:
 class Stack:
     def __init__(self):
         self.top = None
+        self.tail = None
         self.size = 0
 
     def push(self, data):
@@ -17,8 +18,8 @@ class Stack:
 
     def pop(self):
         if not self.is_empty():
-            popped_node = self.top.data
-            self.top = self.top.next
+            popped_node = self.top
+            self.top = popped_node.next
             popped_node.next = None
             self.size -= 1
             return popped_node.data
@@ -30,9 +31,51 @@ class Stack:
             return self.top.data
         else:
             raise IndexError("peek from an empty stack")
+        
+    def print(self):
+        current_node = self.top
+        result = ''
+
+        while current_node is not None:
+            result = str(current_node.data) + '' + result
+            current_node = current_node.next
+
+        return result
 
     def is_empty(self):
         return self.size == 0
 
     def length(self):
         return self.size
+    
+def infixToPostfix(infix_expression):
+    operators = {'+':1, '-':1, '*':2, '/':2, '(':0}
+    ops = Stack()
+    output = Stack()
+
+    for token in infix_expression:
+        if token.isalnum():
+            output.push(token)
+        elif token == '(':
+            ops.push(token)
+        elif token == ')':
+            while not ops.is_empty() and ops.peek() != '(':
+                output.push(ops.pop())
+            ops.pop()
+        else:
+            while not ops.is_empty and operators[token] <= operators[ops.peek()]:
+                output.push(ops.pop(token))
+            ops.push(token)
+
+    while not ops.is_empty():
+        output.push(ops.pop())
+
+    return output.print()
+
+
+# Example usage:
+infix_expression = "(A+B)*(C+D)"
+print(infixToPostfix(infix_expression)) # Output: "3524-*+"
+
+    
+
